a76c14fd389d51b41eedd831f5b6988cb9401afa
Hassan Bouzlima <bouzlima.h@live.fr>
2021-08-25 13:08:09 +0100

    Add cash selectors

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,10 +1,32 @@
-import { ActionReducerMap } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store';
 import { cashReducer, CashState } from './cash/cash.reducer';
 
 export interface AppState {
   cash: CashState;
 }
 
-export const reducer: ActionReducerMap<AppState, any> = {
+export const reducer: ActionReducerMap<AppState> = {
   cash: cashReducer,
 };
+
+export const selectCashState = createFeatureSelector<CashState>('cash');
+
+export const selectCash = createSelector(
+  selectCashState,
+  (s: CashState) => s?.cash
+);
+
+export const selectSelectedCash = createSelector(
+  selectCashState,
+  (s: CashState) =>
+    s.cash
+      ? Object.assign(
+          {},
+          s.cash.find((cash) => cash._id == s.selectedCash)
+        )
+      : null
+);
