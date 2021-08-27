ac7f61f71a5b73fb398bc195fb742c771b1a9e06
Hassan Bouzlima <bouzlima.h@live.fr>
2021-08-27 17:02:54 +0100

    Update utility.ts

--- src/app/shared/utility.ts
+++ src/app/shared/utility.ts
@@ -1,5 +1,4 @@
 import { Cash } from './Interfaces/Cash';
-import { TotalInOut } from './Interfaces/TotalInOut';
 
 export function calculateBalance(startingIndex, data: Cash[]): Cash[] {
   let newCash = [...data];
@@ -30,14 +29,4 @@ export function sortFunction(a: Cash, b: Cash) {
   if (b.date < a.date) return -1;
   if (b.date > a.date) return 1;
   return 0;
-}
-
-export function totalInOut(data: Cash[]): TotalInOut {
-  const totalInOut = { in: 0, out: 0 };
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].in == 1) {
-      totalInOut.in += data[i].amount;
-    } else totalInOut.out += data[i].amount;
-  }
-  return totalInOut;
 }
