cb2c85ed8f9c6c9ae8ac516014322fc8b940cb12
Hassan Bouzlima <bouzlima.h@live.fr>
2023-08-09 13:50:13 +0100

    Add prod env variables :rocket:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  userBaseUrl: 'https://expense-tracker-kudy.onrender.com/user',
+  cashBaseUrl: 'https://expense-tracker-kudy.onrender.com/expense',
+  settingBaseUrl: 'https://expense-tracker-kudy.onrender.com/setting',
 };
