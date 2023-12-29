fea7e51a2af4ffcc5eca2c810781cdeff85142dd
Hassan Bouzlima <bouzlima.h@live.fr>
2023-12-29 17:10:16 +0100

    Update deprecated code

--- src/app/shared/services/modal.service.ts
+++ src/app/shared/services/modal.service.ts
@@ -1,9 +1,4 @@
-import {
-  ComponentFactoryResolver,
-  ComponentRef,
-  Injectable,
-  ViewContainerRef,
-} from '@angular/core';
+import { ComponentRef, Injectable, ViewContainerRef } from '@angular/core';
 import { Subject } from 'rxjs';
 import { ModalComponent } from 'src/app/home/modal/modal.component';
 
@@ -11,15 +6,14 @@ import { ModalComponent } from 'src/app/home/modal/modal.component';
 export class ModalService {
   private componentRef: ComponentRef<ModalComponent>;
   private componentSubscriber: Subject<string>;
-  constructor(private resolver: ComponentFactoryResolver) {}
+  constructor() {}
 
   open(
     componentContainer: ViewContainerRef,
     modalTitle: string,
     modalBody: string
   ) {
-    let factory = this.resolver.resolveComponentFactory(ModalComponent);
-    this.componentRef = componentContainer.createComponent(factory);
+    this.componentRef = componentContainer.createComponent(ModalComponent);
     this.componentRef.instance.closeme.subscribe(() => this.closeMe());
     this.componentRef.instance.confirmme.subscribe(() => this.confirm());
     this.componentRef.instance.title = modalTitle;
